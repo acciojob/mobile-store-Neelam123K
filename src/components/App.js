20cf0df8bf7f52715d6b5f4a1b59970ebe37af4d
Neelam123K <153060910+Neelam123K@users.noreply.github.com>
2025-03-09 15:30:35 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,19 @@
-
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ProductList from './ProductList';
+import ProductDetails from './ProductDetails';
 import './../styles/App.css';
+import AdminPanel from "./AdminPanel";
 
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ProductList />} />
+        <Route path="/products/:id" element={<ProductDetails />} />
+        <Route path="/admin" element={<AdminPanel />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/data/products.js
@@ -0,0 +1,7 @@
+const products = [
+  { id: 1, name: "iPhone 13", price: 999, description: "Apple's latest iPhone.", image: "iphone.jpg" },
+  { id: 2, name: "Samsung Galaxy S22", price: 899, description: "Samsung flagship.", image: "galaxy.jpg" },
+  { id: 3, name: "Google Pixel 6", price: 799, description: "Pure Android experience.", image: "pixel.jpg" }
+];
+
+export default products;
